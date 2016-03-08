0c45c88a684766b9506f72b9dab8b4abe683e5b2
jaindeepali <djdeepalijain811@gmail.com>
2016-03-08 17:02:54 +0530

    Add fastICA with input and output plots

---
+++ README.md
@@ -0,0 +1,7 @@
+### Cocktail Party
+
+Blind source separation of linear mixture of sound signals using fast Independent Component Analysis Algorithm.
+
+#### Dependencies:
+* [fastICA](https://cran.r-project.org/web/packages/fastICA/index.html)
+* [tuneR](https://cran.r-project.org/web/packages/tuneR/index.html)
---
+++ separation.R
@@ -0,0 +1,48 @@
+library('fastICA')
+library('tuneR')
+
+## Input Sources
+
+S1 <- readWave('sound files/mixed sources/X1_linear.wav')
+S2 <- readWave('sound files/mixed sources/X2_linear.wav')
+X <- cbind(S1@left, S2@left)
+
+## In case of using original sources
+
+# S <- cbind(S1@left, S2@left)
+# A <- matrix(c(0.291, 0.6557, -0.5439, 0.5572), 2, 2)
+# X <- S %*% A
+
+## Fast ICA
+
+a <- fastICA(X, 2, alg.typ = "parallel", fun = "logcosh", alpha = 1,
+             method = "R", row.norm = FALSE, maxit = 200,
+             tol = 0.0001, verbose = TRUE)
+
+## Plot Signals
+
+par(mfcol = c(2, 3))
+
+# In case of using original sources:
+
+# plot(1:nrow(S), S[,1 ], type = "l", main = "Original Signals",
+#      xlab = "", ylab = "")
+# plot(1:nrow(S), S[,2 ], type = "l", xlab = "", ylab = "")
+
+plot(1:nrow(X), X[,1 ], type = "l", main = "Mixed Signals",
+     xlab = "", ylab = "")
+plot(1:nrow(X), X[,2 ], type = "l", xlab = "", ylab = "")
+
+plot(1:nrow(a$S), a$S[,1 ], type = "l", main = "ICA source estimates",
+     xlab = "", ylab = "")
+plot(1:nrow(a$S), a$S[,2], type = "l", xlab = "", ylab = "")
+
+## Save output signals
+
+out1 <- Wave(left = a$S[,1], samp.rate = S1@samp.rate, bit = S1@bit)
+out2 <- Wave(left = a$S[,2], samp.rate = S1@samp.rate, bit = S1@bit)
+out1 <- normalize(out1, unit = '8')
+out2 <- normalize(out2, unit = '8')
+
+writeWave(out1, filename = 'sound files/output/out1.wav')
+writeWave(out2, filename = 'sound files/output/out2.wav')
