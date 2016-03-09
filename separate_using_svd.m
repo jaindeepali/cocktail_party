2f244efc861a44e153d8e5aff2a3816c39ed9b4e
Divij Bindlish <dvjbndlsh93@gmail.com>
2016-03-10 03:22:54 +0530

    Reorganize sound files

--- separate_using_svd.m
+++ separate_using_svd.m
@@ -7,25 +7,28 @@ function separateUsingSVD(sampleNumber)
 
     numSamples = size(readdir(mixedSourcesDir), 1) - 2;
 
-    [mixedSignal, fs] = audioread(strcat(mixedSourcesDir, '1.wav'));
-    [originalSignal, fs] = audioread(strcat(originalSourcesDir, '1.wav'));
+    [mixedSignal1, fs] = audioread(strcat(mixedSourcesDir, '1.wav'));
+    [originalSignal1, fs] = audioread(strcat(originalSourcesDir, '1.wav'));
+    [mixedSignal2, fs] = audioread(strcat(mixedSourcesDir, '2.wav'));
+    [originalSignal2, fs] = audioread(strcat(originalSourcesDir, '2.wav'));
 
-    mixedSignalList = [mixedSignal];
-    originalSignalList = [originalSignal];
+    size(mixedSignal1)
+    size(mixedSignal2)
+    size(originalSignal1)
+    size(originalSignal2)
 
-    for i = 2:numSamples
-        [mixedSignal, fs] = audioread(strcat(mixedSourcesDir, num2str(i), '.wav'));
-        [originalSignal, fs] = audioread(strcat(originalSourcesDir, num2str(i), '.wav'));
-
-        mixedSignalList(:, i) = mixedSignal;
-        originalSignalList(:, i) = originalSignal;
-    endfor
+    mixedSignalList = [mixedSignal1, mixedSignal2];
+    originalSignalList = [originalSignal1, originalSignal2];
 
     xx = mixedSignalList';
     yy = sqrtm(inv(cov(xx')))*(xx-repmat(mean(xx,2),1,size(xx,2)));
     [W,s,v] = svd((repmat(sum(yy.*yy,1),size(yy,1),1).*yy)*yy');
 
     a = W*xx;
+
+    output1 = a(1, :)';
+    output2 = a(2, :)';
+    outputList = [output1, output2];
 
     numGraph = 1;
     for i = 1:numSamples
@@ -38,13 +41,28 @@ function separateUsingSVD(sampleNumber)
         title(strcat('Mixed wave', ' ', num2str(i)));
 
         subplot(3, numSamples, numGraph + numSamples * 2);
-        plot(a(i, :));
+        plot(outputList(:, i));
         title(strcat('Output wave', ' ', num2str(i)));
 
         audiowrite(strcat(outputDir, num2str(i), '.wav'), a(i, :), fs);
 
         numGraph += 1;
     endfor
+
+    originalSignal1 = originalSignal1 / norm(originalSignal1);
+    originalSignal2 = originalSignal2 / norm(originalSignal2);
+    output1 = output1 / norm(output1);
+    output2 = output2 / norm(output2);
+
+    e1 = abs((abs(originalSignal1) - abs(output1)) ./ abs(originalSignal1));
+    e1 = e1(isfinite(e1));
+    e1 = sqrt(mean(e1 .^ 2));
+    e2 = abs((abs(originalSignal2) - abs(output2)) ./ abs(originalSignal2));
+    e2 = e2(isfinite(e2));
+    e2 = sqrt(mean(e2 .^ 2));
+    'ERROR'
+    abs(mean([e1; e2]))
+
 endfunction
 
 separateUsingSVD(3);
