85fa1c646a412d4f00749ad763e56adbf2611642
Divij Bindlish <dvjbndlsh93@gmail.com>
2016-03-09 23:34:12 +0530

    Rename files

--- slow.m
+++ slow.m
@@ -1,8 +1,7 @@
-[orig1, _1] = audioread('sound files/original sources/source1.wav');
-[orig2, _2] = audioread('sound files/original sources/source2.wav');
-
-[x1, Fs1] = audioread('sound files/mixed sources/X1_linear.wav');
-[x2, Fs2] = audioread('sound files/mixed sources/X2_linear.wav');
+[orig1, Fso1] = audioread('sound files/original sources/source2a.wav');
+[orig2, Fso2] = audioread('sound files/original sources/source2b.wav');
+[x1, Fs1] = audioread('sound files/mixed sources/mix2a.wav');
+[x2, Fs2] = audioread('sound files/mixed sources/mix2b.wav');
 xx = [x1, x2]';
 yy = sqrtm(inv(cov(xx')))*(xx-repmat(mean(xx,2),1,size(xx,2)));
 [W,s,v] = svd((repmat(sum(yy.*yy,1),size(yy,1),1).*yy)*yy');
@@ -11,7 +10,7 @@ a = W*xx; %W is unmixing matrix
 subplot(3,2,1); plot(x1); title('mixed audio - mic 1');
 subplot(3,2,2); plot(x2); title('mixed audio - mic 2');
 subplot(3,2,3); plot(a(1,:), 'g'); title('unmixed wave 1');
-subplot(3,2,4); plot(a(2,:), 'r'); title('unmixed wave 2');
+subplot(3,2,4); plot(a(2,:),'r'); title('unmixed wave 2');
 subplot(3,2,5); plot(orig1); title('original wave 1');
 subplot(3,2,6); plot(orig2); title('original wave 2');
 
